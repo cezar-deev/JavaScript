3e248c599e7a10d21ebd6a5aa6e5fd3a4703d744
Cezar Cardoso <cezar.deev@gmail.com>
2021-10-26 15:29:38 -0300

    Atualizar Exercicio

--- Exercicios/Exec_[Estrut.Condic].js
+++ Exercicios/Exec_[Estrut.Condic].js
@@ -98,3 +98,147 @@ var status = nota > 7 ? "Aprovado" : "Reprovado";
 var mensagem = (mes == 11 || mes == 12) ? "Promoção" : "Preço normal";
 
 
+//--------------------------------------------------------------------
+
+
+let montadora = "Fiat";
+
+ if (montadora == "Nissan") {
+    console.log("Nissan");
+ }
+   else if (montadora == "Honda") {
+   console.log("Honda");
+ }
+ else
+ {
+    console.log("Fiat");
+ }
+
+
+//--------------------------------------------------------------------
+
+
+ var dia_semana = 6;
+
+switch (dia_semana) {
+    
+case 0:
+    console.log("Domingo");   
+break;
+    
+case 1:
+    console.log("Segunda-feira");   
+break;
+    
+case 2:
+    console.log("Terça-feira");  
+break;
+    
+case 3:
+    console.log("Quarta-feira");  
+break;
+    
+case 4:
+    console.log("Quinta-feira");   
+break;
+    
+case 5:
+    console.log("Sexta-feira");  
+break;
+    
+case 6:
+    console.log("Sábado");   
+break;
+}
+
+
+//--------------------------------------------------------------------
+
+
+var consulta = 1;
+
+switch (consulta) {
+        case 0:
+        console.log("Hoje");
+        break; 
+        
+        case 1:
+        console.log("Amanhã");      
+        break;
+    
+        default:
+        console.log("Sem compromisso");
+}
+
+
+//--------------------------------------------------------------------
+//Complete corretamente o código abaixo para que o resultado impresso seja "Curso de JavaScript":
+
+const nome = 'Ana';
+
+switch (nome) {
+    
+    case "Ana":    
+    case "Alberto":
+      console.log("Curso de JavaScript");       
+    break;
+}
+
+
+//--------------------------------------------------------------------
+//Complete corretamente o código abaixo para que o resultado impresso seja "Gears of War":
+
+let plataforma = 'xbox';
+let jogo = "";
+
+switch (plataforma) {  
+  case 'xbox':    
+  jogo = 'Gears of War';
+  break;
+  
+  case "":
+  jogo = 'Não temos jogos para seu console';
+}
+
+console.log(jogo);
+
+//--------------------------------------------------------------------
+//Reescreva os códigos abaixo alternando apenas as partes incorretas:
+
+//Curto-circuito
+let status = false;
+status && console.log("Aprovado");
+
+//If ternário
+let status = false;
+let resultado = (status) ? "Aprovado" : "Reprovado";
+
+
+//--------------------------------------------------------------------
+//Mudando de If termanio para If simples
+
+let status = true;
+
+   if (status == true) {  
+   console.log("Compra aprovada");
+}
+
+
+/--------------------------------------------------------------------
+//Complete o código abaixo de acordo com as instruções para converter corretamente o if em if ternário:
+
+let produto = "Geladeira";
+let retorno = "";
+
+if(produto == "Geladeira")
+{
+    retorno = "desconto";
+}
+else
+{
+    retorno = "normal";
+}
+
+// convertendo
+
+let retorno = ( produtto == "Geladeira" ) ? "desconto" : "normal"
