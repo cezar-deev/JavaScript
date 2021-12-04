3ff4ba3ca88ffa5da6ab73c767a5997a44b795ab
Cezar Cardoso <cezar.deev@gmail.com>
2021-12-04 17:28:23 -0300

    Atualizar

--- Projetos/05-Proj_Analizador de Numeros/script.js
+++ Projetos/05-Proj_Analizador de Numeros/script.js
@@ -21,8 +21,11 @@ function inLista(n, l){
 }
 
 function adicionar () {
-    if(isNumero(num.value) && !inLista(num.value,valores)) {
-        window.alert('Tudo Ok!')
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         window.alert('Valor Invalido ou já encontrado')
 
